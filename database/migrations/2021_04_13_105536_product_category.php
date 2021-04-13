4a62360e7099d65bca35b585ad520e68c6ab5ed6
Mayur Nadiya <mayur.nadiya.php@gmail.com>
2021-04-13 16:43:04 +0530

    Create Migration for Product Category table.

---
+++ database/migrations/2021_04_13_105536_product_category.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProductCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_category', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('icon');
+            $table->string('bg_color');
+            $table->foreignId('parent_category_id')->constrained('product_category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
